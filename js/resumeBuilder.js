86df3d60b8055fb19bf16da430af3d2f6e8acd80
Doug Conner <dconner@connerbase.com>
2015-03-06 15:22:26 -0800

    add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,13 @@
 //$("#main").append("Doug Conner");
 
 // [string].replace([old], [new])
-var awesomeThoughts = "I am Doug and I am AWESOME!";
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
+// var awesomeThoughts = "I am Doug and I am AWESOME!";
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// $("#main").append(funThoughts);
 //console.log(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Doug Conner");
+var formattedRole = HTMLheaderRole.replace("%data%", "web Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
