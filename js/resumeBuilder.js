a6917fa1ee203d1de168c912fdb6eb4753ebff86
Doug Conner <dconner@connerbase.com>
2015-03-07 17:03:56 -0800

    Code to Cap 1st letter of first name and all of last name. Temporary

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,3 +11,23 @@ var formattedRole = HTMLheaderRole.replace("%data%", "web Developer");
 
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
+
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+    var nameString = finalName.split(" ");
+    nameString[0] = nameString[0].toLowerCase();
+    var firstLetter = nameString[0][0].toUpperCase();
+    nameString[0] = nameString[0].slice(1);
+    nameString[1] = nameString[1].toUpperCase();
+    finalName = firstLetter + nameString[0] + " " + nameString[1];
+
+
+    // Don't delete this line!
+    return finalName;
+};
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
