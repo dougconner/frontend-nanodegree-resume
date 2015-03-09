3761e8799621965398f14d90e4905aa143f13dc1
Doug Conner <dconner@connerbase.com>
2015-03-09 11:38:04 -0700

    add practice with objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,24 +9,55 @@
 var bio = {
 	"name" : "Doug Conner",
 	"role" : "Web Developer",
-	"contact" : "dconner@connerbase.com",
-	"picture" : "images/me.jpg",
+	"contact" : {
+		"mobile" : "805-440-6737",
+		"email" : "dconner@connerbase.com",
+		"github" : "https://github.com/dougconner/",
+		"location" : "Atascadero, CA"
+	},
 	"welcomeMsg" : "Hi",
-	"skills" : "skills"
+	"skills" : [
+		"problem solver", " make it work"
+	],
+	"picture" : "images/me.jpg"
 };
+
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contact, "%contact%");
-//formattedContact = HTMLcontactGeneric.replace("%contact%", bio.contact);
+var formattedContactGeneric = HTMLcontactGeneric.replace("%contact%", "contact information");
+var formattedContactGeneric2 = formattedContactGeneric.replace("%data%", ":");
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
 var formattedPicture = HTMLbioPic.replace("%data%", bio.picture);
 var formattedWelcomMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
 var formattedSkills =  HTMLskills.replace("%data%", bio.skills);
 
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
-$("#header").append(formattedContact);
+$("#header").append(formattedContactGeneric2);
+$("#header").append(formattedMobile);
+$("#header").append(formattedEmail);
 $("#header").append(formattedPicture);
 $("#header").append(formattedWelcomMsg);
 $("#header").append(formattedSkills);
+
+var work = {};
+work.position = "engineer";
+work.employer = "conner engineering";
+work.yearsWorked = "5";
+work.city = "Atascadero";
+
+var formattedWorkPosition = HTMLworkTitle.replace("%data%", work["position"]);
+
+$("#header").append(formattedWorkPosition);
+
+var education = {};
+education["school"] = "Udacity";
+education["yearsAttended"] = "1";
+education["city"] = "online";
+
+var formattedSchoolName = HTMLschoolName.replace("%data%", education.school);
+
+$("#header").append(formattedSchoolName);
 
